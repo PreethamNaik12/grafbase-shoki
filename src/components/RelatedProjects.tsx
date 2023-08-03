65de4f106ba26989415ae3a89ec980cf91a6021c
Preetham <preethamgr123@gmail.com>
2023-08-04 03:46:26 +0530

    key

--- src/components/RelatedProjects.tsx
+++ src/components/RelatedProjects.tsx
@@ -15,6 +15,8 @@ const RelatedProjects = async ({ userId, projectId}: Props) => {
 
     if (filteredProjects?.length === 0) return null;
 
+    let i = 0;
+
     return (
         <section className="flex flex-col mt-32 w-full">
         <div className="flexBetween">
@@ -31,7 +33,7 @@ const RelatedProjects = async ({ userId, projectId}: Props) => {
 
         <div className="related_projects-grid">
             {filteredProjects?.map(({ node }: { node: ProjectInterface }) => (
-                <div className="flexCenter related_project-card drop-shadow-card">
+                <div key = {i++} className="flexCenter related_project-card drop-shadow-card">
                     <Link href={`/project/${node?.id}`} className="flexCenter group relative w-full h-full">
                         <Image src={node?.image} width={414} height={314} className="w-full h-full object-cover rounded-2xl" alt="project image" />
         
